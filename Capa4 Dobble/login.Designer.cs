56ba12d4c1e109bd573813cd5042467c327db6de
Adriel Casco <adriel369@gmail.com>
2019-11-23 23:03:18 -0600

    Agregar archivos de proyecto.

---
+++ Capa4 Dobble/login.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Capa4_Dobble
+{
+    partial class login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            this.panelLog = new System.Windows.Forms.Panel();
+            this.panelRegistro = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btRegistro = new System.Windows.Forms.Button();
+            this.textConstrasennaRegsitro = new System.Windows.Forms.TextBox();
+            this.textUsuarioRegistro = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panelInicioSesion = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btIngresar = new System.Windows.Forms.Button();
+            this.contrasennaUsuario = new System.Windows.Forms.TextBox();
+            this.usuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btRegistrarse = new System.Windows.Forms.Button();
+            this.btIniciarSesion = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelLog.SuspendLayout();
+            this.panelRegistro.SuspendLayout();
+            this.panelInicioSesion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelLog
+            // 
+            this.panelLog.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panelLog.Controls.Add(this.panelRegistro);
+            this.panelLog.Controls.Add(this.panelInicioSesion);
+            this.panelLog.Controls.Add(this.btRegistrarse);
+            this.panelLog.Controls.Add(this.btIniciarSesion);
+            this.panelLog.Controls.Add(this.label3);
+            this.panelLog.Controls.Add(this.pictureBox1);
+            this.panelLog.Location = new System.Drawing.Point(1, 1);
+            this.panelLog.Name = "panelLog";
+            this.panelLog.Size = new System.Drawing.Size(565, 395);
+            this.panelLog.TabIndex = 0;
+            // 
+            // panelRegistro
+            // 
+            this.panelRegistro.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelRegistro.Controls.Add(this.label6);
+            this.panelRegistro.Controls.Add(this.btRegistro);
+            this.panelRegistro.Controls.Add(this.textConstrasennaRegsitro);
+            this.panelRegistro.Controls.Add(this.textUsuarioRegistro);
+            this.panelRegistro.Controls.Add(this.label4);
+            this.panelRegistro.Controls.Add(this.label5);
+            this.panelRegistro.Controls.Add(this.button3);
+            this.panelRegistro.Location = new System.Drawing.Point(238, 73);
+            this.panelRegistro.Name = "panelRegistro";
+            this.panelRegistro.Size = new System.Drawing.Size(216, 201);
+            this.panelRegistro.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(11, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = " Registrar usuario";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // btRegistro
+            // 
+            this.btRegistro.Location = new System.Drawing.Point(118, 154);
+            this.btRegistro.Name = "btRegistro";
+            this.btRegistro.Size = new System.Drawing.Size(75, 23);
+            this.btRegistro.TabIndex = 5;
+            this.btRegistro.Text = "Registrarse";
+            this.btRegistro.UseVisualStyleBackColor = true;
+            this.btRegistro.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textConstrasennaRegsitro
+            // 
+            this.textConstrasennaRegsitro.Location = new System.Drawing.Point(28, 128);
+            this.textConstrasennaRegsitro.Name = "textConstrasennaRegsitro";
+            this.textConstrasennaRegsitro.Size = new System.Drawing.Size(153, 20);
+            this.textConstrasennaRegsitro.TabIndex = 4;
+            // 
+            // textUsuarioRegistro
+            // 
+            this.textUsuarioRegistro.Location = new System.Drawing.Point(28, 80);
+            this.textUsuarioRegistro.Name = "textUsuarioRegistro";
+            this.textUsuarioRegistro.Size = new System.Drawing.Size(153, 20);
+            this.textUsuarioRegistro.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Contraseña:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = " Nombre de usuario: ";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(183, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 27);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panelInicioSesion
+            // 
+            this.panelInicioSesion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelInicioSesion.Controls.Add(this.label7);
+            this.panelInicioSesion.Controls.Add(this.btIngresar);
+            this.panelInicioSesion.Controls.Add(this.contrasennaUsuario);
+            this.panelInicioSesion.Controls.Add(this.usuario);
+            this.panelInicioSesion.Controls.Add(this.label2);
+            this.panelInicioSesion.Controls.Add(this.label1);
+            this.panelInicioSesion.Controls.Add(this.button1);
+            this.panelInicioSesion.Location = new System.Drawing.Point(42, 31);
+            this.panelInicioSesion.Name = "panelInicioSesion";
+            this.panelInicioSesion.Size = new System.Drawing.Size(206, 197);
+            this.panelInicioSesion.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(10, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Iniciar sesión";
+            // 
+            // btIngresar
+            // 
+            this.btIngresar.Location = new System.Drawing.Point(118, 154);
+            this.btIngresar.Name = "btIngresar";
+            this.btIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btIngresar.TabIndex = 5;
+            this.btIngresar.Text = "Ingresar";
+            this.btIngresar.UseVisualStyleBackColor = true;
+            this.btIngresar.Click += new System.EventHandler(this.btIngresar_Click);
+            // 
+            // contrasennaUsuario
+            // 
+            this.contrasennaUsuario.Location = new System.Drawing.Point(28, 128);
+            this.contrasennaUsuario.Name = "contrasennaUsuario";
+            this.contrasennaUsuario.Size = new System.Drawing.Size(153, 20);
+            this.contrasennaUsuario.TabIndex = 4;
+            // 
+            // usuario
+            // 
+            this.usuario.Location = new System.Drawing.Point(28, 80);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(153, 20);
+            this.usuario.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " Nombre de usuario: ";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(178, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btRegistrarse
+            // 
+            this.btRegistrarse.Location = new System.Drawing.Point(228, 225);
+            this.btRegistrarse.Name = "btRegistrarse";
+            this.btRegistrarse.Size = new System.Drawing.Size(156, 68);
+            this.btRegistrarse.TabIndex = 2;
+            this.btRegistrarse.Text = "Registrarse";
+            this.btRegistrarse.UseVisualStyleBackColor = true;
+            this.btRegistrarse.Click += new System.EventHandler(this.btRegistrarse_Click);
+            // 
+            // btIniciarSesion
+            // 
+            this.btIniciarSesion.Location = new System.Drawing.Point(228, 128);
+            this.btIniciarSesion.Name = "btIniciarSesion";
+            this.btIniciarSesion.Size = new System.Drawing.Size(156, 68);
+            this.btIniciarSesion.TabIndex = 1;
+            this.btIniciarSesion.Text = "Iniciar sesión";
+            this.btIniciarSesion.UseVisualStyleBackColor = true;
+            this.btIniciarSesion.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Maiandra GD", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(172, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(291, 77);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "SPOT IT!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 154);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(286, 241);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 396);
+            this.Controls.Add(this.panelLog);
+            this.Name = "login";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelLog.ResumeLayout(false);
+            this.panelLog.PerformLayout();
+            this.panelRegistro.ResumeLayout(false);
+            this.panelRegistro.PerformLayout();
+            this.panelInicioSesion.ResumeLayout(false);
+            this.panelInicioSesion.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLog;
+        private System.Windows.Forms.Button btIniciarSesion;
+        private System.Windows.Forms.Button btRegistrarse;
+        private System.Windows.Forms.Panel panelInicioSesion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox contrasennaUsuario;
+        private System.Windows.Forms.TextBox usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btIngresar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelRegistro;
+        private System.Windows.Forms.Button btRegistro;
+        private System.Windows.Forms.TextBox textConstrasennaRegsitro;
+        private System.Windows.Forms.TextBox textUsuarioRegistro;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Capa4 Dobble/login.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa4_Dobble
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+
+            InitializeComponent();
+            panelInicioSesion.Visible = false;
+            panelRegistro.Visible = false;
+            contrasennaUsuario.Text = "";
+            contrasennaUsuario.PasswordChar = '*';
+            contrasennaUsuario.MaxLength = 14;
+            textConstrasennaRegsitro.Text = "";
+            textConstrasennaRegsitro.PasswordChar = '*';
+            textConstrasennaRegsitro.MaxLength = 14;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panelInicioSesion.Visible = true;
+        }
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            panelInicioSesion.Visible = false;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+                    }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btRegistrarse_Click(object sender, EventArgs e)
+        {
+            panelRegistro.Visible = true;
+            
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panelRegistro.Visible = false; 
+        }
+
+        private void btIngresar_Click(object sender, EventArgs e)
+        {
+            var myForm = new perfilJugador();
+            myForm.Show();
+        }
+    }
+}
