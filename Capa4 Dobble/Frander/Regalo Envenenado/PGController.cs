97b20115a346a19015b2c47300e3b3ec2142baaf
JosephMen <franderms@gmail.com>
2019-11-25 01:20:42 -0600

    <<<  Poisoned Gift >>>>>

---
+++ Capa4 Dobble/Frander/Regalo Envenenado/PGController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa4_Dobble.Frander.Regalo_Envenenado
+{
+    class PGController
+    {
+        PGController()
+        {
+            poisonedGift titulo = new poisonedGift();
+            titulo.Show();
+            Carta me = new Carta();
+            Carta jogador1 = new Carta();
+            Carta jogador2 = new Carta();
+            Carta jogador3 = new Carta();
+        }
+    }
+}
--- Capa4 Dobble/login.cs
+++ Capa4 Dobble/login.cs
@@ -57,6 +57,7 @@ namespace Capa4_Dobble
 
         private void btRegistrarse_Click(object sender, EventArgs e)
         {
+            /*
             panelRegistro.Visible = true;
             int[] cartas = { 0, 44, 31, 7, 25, 50, 4, 41 };
             Carta ventadaCard = new Carta();
@@ -64,6 +65,10 @@ namespace Capa4_Dobble
             //ventadaCard.SetDraws(cartas);
             ventadaCard.updateDraws(cartas);
             ventadaCard.Show();
+            */
+            Frander.Regalo_Envenenado.poisonedGift RegaloEnvenendo = new Frander.Regalo_Envenenado.poisonedGift();
+
+
         }
 
         private void label6_Click(object sender, EventArgs e)
